3372510fd9ff6391ae95a00d3d843263421db3b1
diegorufe <diegorufe@gmail.com>
2020-06-06 08:10:49 +0200

    Added mehtods to kwow map is empty or not

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # RFJVUtils
 
+## Collections
+
+- Check is empty or null
+- Check is not empty and not null
+
+## Map
+
+- Check is empty or null
+- Check is not empty and not null
+
 ## Stock
 
 - Stock turnover index = (costPrice * numberOfSoldItems) / averageStockValue
@@ -10,7 +20,4 @@
 - Rate of return on investment (IRR) = (NPV) = - initialOutlay + (netFlow1 / ((1 + interests).pow(1))) + (nnetFlown / ((1 + interests).pow(n))) + .... = 0. 
 - Compound interest=initialOutlay+((1 + interest).pow(unitTime))
 
-## Collections
 
-- Check is empty or null
-- Check is not empty and not null
---
+++ src/main/java/com/rfJVUtils/utils/UtilsMap.java
@@ -0,0 +1,44 @@
+package com.rfJVUtils.utils;
+
+import java.util.Map;
+
+/**
+ * Class utilties for map
+ * 
+ * <p>
+ * Method to knwo size map
+ * 
+ * <ul>
+ * 
+ * <li>{@link #isEmpty(Map)}</li>
+ * <li>{@link #isNotEmpty(Map)}</li>
+ * </ul>
+ * 
+ * @author diego
+ *
+ */
+public class UtilsMap {
+
+	/**
+	 * Method to know map is empty. Map for know is empty is collection is null or
+	 * empty
+	 * 
+	 * @param collection to check
+	 * @return true if is null or empty false if not
+	 */
+	@SuppressWarnings("rawtypes")
+	public static final boolean isEmpty(Map map) {
+		return map == null || map.isEmpty();
+	}
+
+	/**
+	 * Method to know map is not empty. Map for know is not null and not empty
+	 * 
+	 * @param collection to check
+	 * @return true if is not null and not empty false if not
+	 */
+	@SuppressWarnings("rawtypes")
+	public static final boolean isNotEmpty(Map map) {
+		return !UtilsMap.isEmpty(map);
+	}
+}
