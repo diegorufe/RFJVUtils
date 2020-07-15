90242c8db60bca7b9ffeeaa3e2a604d7e8be136c
diegorufe <diegorufe@gmail.com>
2020-07-15 06:40:52 +0200

    Add utilies for exceptions

---
+++ src/main/java/com/rfJVUtils/utils/UtilsExceptions.java
@@ -0,0 +1,49 @@
+package com.rfJVUtils.utils;
+
+/**
+ * Class utilities for exceptions
+ * 
+ * @author diego
+ *
+ */
+public final class UtilsExceptions {
+
+	private UtilsExceptions() {
+
+	}
+
+	/**
+	 * Method for unwrap exception for get cause for this
+	 * 
+	 * @param <T>
+	 * @param exception to unwrap
+	 * @param type      to unwrap exception
+	 * @return exception unwrap if found type else return exception passed in
+	 *         prameter
+	 */
+	public static <T extends Throwable> Throwable unwrap(Throwable exception, Class<T> type) {
+		while (type.isInstance(exception) && exception.getCause() != null) {
+			exception = exception.getCause();
+		}
+
+		return exception;
+	}
+
+	/**
+	 * Method to check exception is instance of other
+	 * 
+	 * @param <T>
+	 * @param exception to check
+	 * @param type      class to other exception to check
+	 * @return True if exception is instance of other
+	 */
+	public static <T extends Throwable> boolean is(Throwable exception, Class<T> type) {
+		for (; exception != null; exception = exception.getCause()) {
+			if (type.isInstance(exception)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
